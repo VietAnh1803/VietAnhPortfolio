f4c37880ccc94ea4ec5bf638e277c73a5ebec131
VietAnh1803 <anh.nguyen18032004@hcmut.edu.vn>
2025-05-09 20:24:13 +0700

    Updates

---
+++ precache-manifest.a3742f1f01cf4e2143a037fa9626c5b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "020ae8d51a2a90a9da2001a010fa46e6",
+    "url": "/VietAnhPortfolio/index.html"
+  },
+  {
+    "revision": "472135a3e529db022704",
+    "url": "/VietAnhPortfolio/static/css/main.db683005.chunk.css"
+  },
+  {
+    "revision": "ec0935851c1edf4cb708",
+    "url": "/VietAnhPortfolio/static/js/2.9e11b540.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/VietAnhPortfolio/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "472135a3e529db022704",
+    "url": "/VietAnhPortfolio/static/js/main.bdf24de9.chunk.js"
+  },
+  {
+    "revision": "b9b539887d80d5a7955d",
+    "url": "/VietAnhPortfolio/static/js/runtime~main.69348748.js"
+  },
+  {
+    "revision": "9c3e1ea1489d072a8e6cbe4a175edf20",
+    "url": "/VietAnhPortfolio/static/media/avatar.9c3e1ea1.jpg"
+  }
+]);
